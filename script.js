ce8f6d30d695f7cbe6871e2ff49cd59fbd157bce
nicolamonton26 <nicolarosal26@gmail.com>
2026-02-18 08:33:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function setMood(emoji) {
-  document.getElementById("bigEmoji").innerText = emoji;
-}
-
 function submitForm() {
-  alert("Thank you for your feedback!");
+
+  const data = {
+    mood: document.getElementById("bigEmoji").innerText,
+    feedback: document.querySelector("textarea").value,
+    name: document.querySelector("input").value,
+    department: document.querySelector("select").value
+  };
+
+  fetch("https://script.google.com/macros/s/AKfycbxSjPTcG0LO7CEHkE0wfixEfbrptKNqxLQjWPtsNT9XMHz5BqYuAp_f_Iv8kwAsqSfX/exec", {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+  .then(res => res.text())
+  .then(response => {
+    alert("Feedback Submitted!");
+  })
+  .catch(error => {
+    alert("Error submitting feedback");
+  });
 }
